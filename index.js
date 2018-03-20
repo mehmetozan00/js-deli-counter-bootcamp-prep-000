d617ddfd324289acc4297902b0727e3e706d9aee
Ozan Döğüşcü <doguscu_12@hotmail.com>
2018-03-20 00:23:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,14 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return "Welcome, " + name + ". You are number " + katzDeliLine.length + " in line.";
 }
+
+function nowServing(array) {
+  if (array.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  else {
+    var name = array[0];
+    array.slice(0,1);
+    return "Currently serving " + name + ".";
+  }
+}
